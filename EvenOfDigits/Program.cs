37dfb0e5817ae1425e6d3b7553204ffb2264d18e
Михаил Лубенцов <wispergently061@gmail.com>
2024-02-22 21:37:18 +0500

    Создали приложение с использованием двумерного массива и познакомились со строками

---
+++ EvenOfDigits/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+
+Random random = new Random();
+int[,] CreateMatrix(int rowCount, int columsCount){
+
+    int[,] matrix = new int[rowCount, columsCount];
+
+    for(int i = 0; i < matrix.GetLength(0); i++){
+
+        for(int j = 0; j < matrix.GetLength(1); j++){
+
+            matrix[i,j] = random.Next(1, 1000);
+
+        }
+    }
+
+    return matrix;
+}
+
+int[,] matrix = CreateMatrix(3, 4);
+ShowMatrix(matrix);
+
+
+foreach(int elem in matrix){
+
+    if(isInteresting(elem) == true){
+        System.Console.WriteLine(elem);
+    }
+}
+
+bool isInteresting(int value){
+
+   int sumOfDigits = GetSumOFDigits(value);
+   
+   if(sumOfDigits % 2 == 0){
+    return true;
+   }else{
+    return false;
+   }
+}
+
+int GetSumOFDigits(int value){
+    int sum = 0;
+
+    while(value > 0){
+        sum = sum + value % 10;
+        value = value / 10;
+    }
+    return sum;
+}
+
+void ShowMatrix(int[,] matrix){
+
+    for(int i = 0; i < matrix.GetLength(0); i++){
+
+        for(int j = 0; j < matrix.GetLength(1); j++){
+
+            System.Console.Write($"{matrix[i,j]} ");
+            
+        }
+        System.Console.WriteLine(" ");
+    }
+}
---
+++ Strings/Program.cs
@@ -0,0 +1,24 @@
+﻿//Способы создания строк:
+//Прямая передача строкгово значения в переменную типа string
+string str = "hello";
+//Создание строки через массив символов и вызова контруктора String
+char[] char_str = { 'h', 'e', 'l', 'l', 'o'};
+string str2 = new String(char_str);
+//Через повторение символа
+string str3 = new String('a', 6);//результат будет "aaaaaa"
+
+System.Console.WriteLine("Введите строку: ");
+string input = Console.ReadLine();
+string result = GetLettersFromString(input);
+System.Console.WriteLine(result);
+
+string GetLettersFromString(string msg){
+    string letters = "";
+    foreach(char e in msg){
+        
+        if(char.IsAsciiLetter(e) == true){
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
